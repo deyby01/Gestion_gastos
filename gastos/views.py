86453d1fb5bcbd9fd3b7deeeeff7dd75713308a6
deyby01 <camachodeybygerman@gmail.com>
2025-05-19 19:50:56 -0400

    Se agrega grafico, busqueda por cadena y por fecha

--- gastos/views.py
+++ gastos/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Gasto
 from .forms import GastoForm
+import json
+from datetime import datetime
+
+def obtener_datos_gastos():
+    gastos = Gasto.objects.all()
+    return json.dumps([{"nombre": g.nombre, "monto": float(g.monto)} for g in gastos])
 
 def index(request):
-    if request.method == "POST":
-        form = GastoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("index")  # Redirige a la página principal después de guardar
+    query = request.GET.get("q", "")
+    fecha_inicio = request.GET.get("fecha_inicio", "")
+    fecha_fin = request.GET.get("fecha_fin", "")
 
-    else:
-        form = GastoForm()
+    gastos = Gasto.objects.all()
 
-    gastos = Gasto.objects.all()  # Obtiene todos los gastos guardados
-    return render(request, "index.html", {"form": form, "gastos": gastos})
+    if query:
+        gastos = gastos.filter(nombre__icontains=query)
+
+    if fecha_inicio and fecha_fin:
+        fecha_inicio = datetime.strptime(fecha_inicio, "%Y-%m-%d")
+        fecha_fin = datetime.strptime(fecha_fin, "%Y-%m-%d")
+        gastos = gastos.filter(fecha__range=[fecha_inicio, fecha_fin])
+
+    gastos_json = obtener_datos_gastos()
+
+    return render(
+        request, "gastosp/index.html", 
+        {"form": GastoForm(), "gastos": gastos, "gastos_json": gastos_json, "query": query}
+    )
