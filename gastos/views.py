95ad1682dc6fb6594e3fea5674358444242915c4
deyby01 <camachodeybygerman@gmail.com>
2025-05-20 15:15:02 -0400

    se agrega la interfaz de usuario y se mejora la presentacion

--- gastos/views.py
+++ gastos/views.py
@@ -3,29 +3,59 @@ from .models import Gasto
 from .forms import GastoForm
 import json
 from datetime import datetime
+from collections import defaultdict
+import json
+from django.http import JsonResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+def obtener_datos_por_categoria():
+    gastos = Gasto.objects.all()
+    categorias = {}
+
+    for gasto in gastos:
+        if gasto.categoria in categorias:
+            categorias[gasto.categoria] += float(gasto.monto)
+        else:
+            categorias[gasto.categoria] = float(gasto.monto)
+
+    return json.dumps([{"categoria": c, "total": categorias[c]} for c in categorias])
+
+def debug_data(request):
+    datos = obtener_datos_por_categoria()
+    return JsonResponse(datos, safe=False)
 
 def obtener_datos_gastos():
     gastos = Gasto.objects.all()
     return json.dumps([{"nombre": g.nombre, "monto": float(g.monto)} for g in gastos])
 
+
+@login_required(login_url="login")
 def index(request):
     query = request.GET.get("q", "")
     fecha_inicio = request.GET.get("fecha_inicio", "")
     fecha_fin = request.GET.get("fecha_fin", "")
 
+    if request.method == "POST":
+        form = GastoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "¡Gasto agregado exitosamente!")
+            return redirect("index")
+
+    form = GastoForm()
     gastos = Gasto.objects.all()
 
     if query:
         gastos = gastos.filter(nombre__icontains=query)
-
     if fecha_inicio and fecha_fin:
-        fecha_inicio = datetime.strptime(fecha_inicio, "%Y-%m-%d")
-        fecha_fin = datetime.strptime(fecha_fin, "%Y-%m-%d")
         gastos = gastos.filter(fecha__range=[fecha_inicio, fecha_fin])
 
-    gastos_json = obtener_datos_gastos()
+    gastos_por_categoria_json = obtener_datos_por_categoria()
 
-    return render(
-        request, "gastosp/index.html", 
-        {"form": GastoForm(), "gastos": gastos, "gastos_json": gastos_json, "query": query}
-    )
+    return render(request, "gastosp/index.html", {
+        "form": form,
+        "gastos": gastos,
+        "gastos_por_categoria_json": gastos_por_categoria_json
+    })
+
--- gestor_gastos/settings.py
+++ gestor_gastos/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "login"
+
 
 # Application definition
 
